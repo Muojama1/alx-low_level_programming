fd2dfd07f56353ce0cd7596aa580e0cac8c3d867
Muojama1 <sandramuojama@gmail.com>
2022-09-08 16:24:06 -0700

    please

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
+/*
+ * File: 5-printf.c
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "with proper grammer, but the outcome is a piece of art," followed by a new line.
  *
  * Return: Always 0 (Success)
  */
