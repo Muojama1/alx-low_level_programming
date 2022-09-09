e8c1f63f7fbfcc9bec6797f02b91a252e1b6529e
Muojama1 <sandramuojama@gmail.com>
2022-09-08 17:22:56 -0700

    bet

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,13 +6,14 @@
 
 /**
  * main - Prints "with proper grammer, but the outcome is
- followed by a new line.
+ *	a piece of art," followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammer, but the outcome is a piece of art, \n");
+
+	return (O);
 }	
