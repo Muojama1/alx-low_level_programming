f560259e40b8b741b505d99f3ed3e7cfa46e327d
Muojama1 <sandramuojama@gmail.com>
2022-09-08 18:09:38 -0700

    God please

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,14 @@
-/*
- * File: 5-printf.c
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammer, but the outcome is
- *	a piece of art," followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-printf("with proper grammer, but the outcome is a piece of art, \n");
-
-	return (O);
-}	
+	return (0);
+}
